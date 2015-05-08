78677ea623872c5a4b903a909045f55284668430
Andiry Xu <jix024@cs.ucsd.edu>
2015-05-07 21:48:53 -0700

    Declare memcpy type

--- xip.c
+++ xip.c
@@ -154,6 +154,22 @@ static inline void pmfs_flush_edge_cachelines(loff_t pos, ssize_t len,
 		pmfs_flush_buffer(start_addr + len, 1, false);
 }
 
+static inline size_t memcpy_to_nvmm(char *kmem, loff_t offset,
+	const char __user *buf, size_t bytes)
+{
+	size_t copied;
+
+	if (support_clwb) {
+		copied = bytes - __copy_from_user(kmem + offset, buf, bytes);
+		pmfs_flush_buffer(kmem + offset, copied, 0);
+	} else {
+		copied = bytes - __copy_from_user_inatomic_nocache(kmem +
+						offset, buf, bytes);
+	}
+
+	return copied;
+}
+
 static ssize_t
 __pmfs_xip_file_write(struct address_space *mapping, const char __user *buf,
           size_t count, loff_t pos, loff_t *ppos)
@@ -187,7 +203,7 @@ __pmfs_xip_file_write(struct address_space *mapping, const char __user *buf,
 
 		PMFS_START_TIMING(memcpy_w_t, memcpy_time);
 		pmfs_xip_mem_protect(sb, xmem + offset, bytes, 1);
-		copied = bytes - __copy_from_user(xmem + offset, buf, bytes);
+		copied = memcpy_to_nvmm((char *)xmem, offset, buf, bytes);
 		pmfs_flush_buffer(xmem + offset, copied, 0);
 		pmfs_xip_mem_protect(sb, xmem + offset, bytes, 0);
 		PMFS_END_TIMING(memcpy_w_t, memcpy_time);
@@ -242,7 +258,7 @@ static ssize_t pmfs_file_write_fast(struct super_block *sb, struct inode *inode,
 
 	PMFS_START_TIMING(memcpy_w_t, memcpy_time);
 	pmfs_xip_mem_protect(sb, xmem + offset, count, 1);
-	copied = count - __copy_from_user(xmem + offset, buf, count);
+	copied = memcpy_to_nvmm((char *)xmem, offset, buf, count);
 	pmfs_flush_buffer(xmem + offset, copied, 0);
 	pmfs_xip_mem_protect(sb, xmem + offset, count, 0);
 	PMFS_END_TIMING(memcpy_w_t, memcpy_time);
