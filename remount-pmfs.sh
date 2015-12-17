1d4aaa371e0044d646b45004a2f5b2a60cd83031
Andiry Xu <jix024@cs.ucsd.edu>
2015-12-17 13:15:17 -0800

    Script fix

--- remount-pmfs.sh
+++ remount-pmfs.sh
@@ -6,7 +6,7 @@ insmod pmfs.ko measure_timing=0
 
 sleep 1
 
-mount -t pmfs -o physaddr=0x400000000 none /mnt/ramdisk
+mount -t pmfs /dev/pmem0 /mnt/ramdisk
 
 #cp test1 /mnt/ramdisk/
 #dd if=/dev/zero of=/mnt/ramdisk/test1 bs=1M count=1024 oflag=direct
